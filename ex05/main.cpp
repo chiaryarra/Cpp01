a9f57b06c6abb2f7d377a3b5bc00b0ba71e83040
lbarreto <luizfernandobarretodasilva@gmail.com>
2025-08-20 20:46:36 -0300

    Feat: Just finished ex05

---
+++ ex05/main.cpp
@@ -0,0 +1,11 @@
+#include "Harl.hpp"
+
+int main(void)
+{
+    Harl mr_harl;
+
+    mr_harl.complain("DEBUG");
+    mr_harl.complain("ERROR");
+    mr_harl.complain("WARNING");
+    mr_harl.complain("INFO");
+}
